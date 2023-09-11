25f1a12299e5bd1578101f801832f8f2cdcc25b7
Vinson Lin <vinsonlin@ucsb.edu>
2023-09-10 17:10:28 -0700

    registration

---
+++ website/forms.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.forms import UserCreationForm # form that creates users
+from django.contrib.auth.models import User
+
+
+class SignUpForm(UserCreationForm):
+    
--- website/views.py
+++ website/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages  # for messages to interact with user/client
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    # check to see if logging in
+    if request.method == 'POST': # this is the method of the request (in home.html we set the form's method as POST)
+        username = request.POST['username'] # this is from the name='password' in home.html
+        password = request.POST['password'] # here we are setting the variables that we are getting from the request
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user) # login user
+            messages.success(request, "Logged in successfully!") # we must still add messages to html
+            return redirect('home') # redirect/return back to home page
+        else:
+            messages.success(request, "There was an error logging in...")
+            return redirect('home')
+
+    else:
+        return render(request, 'home.html', {})
+
+def login_user(request):    # request gets sent to back-end, request is when interacting with webpage
+    pass
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Logged out successfully!")
+    return redirect('home')
+
